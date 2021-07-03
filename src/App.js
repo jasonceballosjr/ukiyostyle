616e1e680c0dc03beb1d904750632c5d9e9bb612
Jason Ceballos <jceballos@icstars.org>
2021-07-03 14:43:11 -0500

    Version 1.1a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from 'react';
-import Products from './components/Products/Products';
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
+import { Products, Navbar, Cart } from './components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  }
+
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  }
+
+  const handleAddToCart = async (productID, quantity) => {
+    const item = await commerce.cart.add(productID, quantity);
+
+    setCart(item.cart);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
+  console.log(cart);
+
   return (
-    <div>
-      <Products />
-    </div>
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Switch>
+          <Route exact path="/">
+            <Products products={products} onAddToCart={handleAddToCart} />
+          </Route>
+          <Route exact path="/cart">
+            <Cart cart={cart} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
-const products = [
-    { id: 1, name: "Nails", description: "Press on nails.", price: '$25' },
-    { id: 2, name: "Custom Nails", description: "Custom styled nails.", price: '$40' },
-];
 
-const Products = () => {
+// const products = [
+//     { id: 1, name: "Nails", description: "Press-On Nails.", price: '$25', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDmXACat6bZgt54BGd_HXGfI8CrqjnE1HEGg&usqp=CAU' },
+//     { id: 2, name: "Custom Nails", description: "Custom Press-On Nails.", price: '$40', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrQAN9fSM7ZqYQqk7Mu6ohUC9tUsqQt3AF1Q&usqp=CAU' },
+// ];
+
+const Products = ({ products, onAddToCart }) => {
+    const classes = useStyles();
+
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} />
+                        <Product product={product} onAddToCart={onAddToCart} />
                     </Grid>
                 ))}
 
